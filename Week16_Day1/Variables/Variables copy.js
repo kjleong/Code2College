cba0fe6d1b686851b5c4a1d48ab621a568602855
Kevin Leong <KevinJ.Leong@gmail.com>
2018-08-25 12:25:34 -0500

    Add files via upload

---
+++ Week16_Day1/Variables/Variables copy.js
@@ -0,0 +1,13 @@
+var color = "Blue";
+
+//Create a variable called carName and assign it the value "Honda" on the same line
+
+//Create a variable called superheroName on one line and assign the value "WonderWoman" on the next line
+
+//Create a variable called myAge and assign it your age as a number
+
+//Remember to use semicolons at the end of the lines
+
+function addStringToId(strToAdd,elementId) {
+	document.getElementById(elementId).innerHTML += ' '+strToAdd
+}
